d9b7cd5848a810b6123a3bfa94680d61a2fb4065
Себия12 <sebiya.bayburtli@gmail.com>
2024-01-14 23:49:09 +0300

    создала схемы

--- book_s/mongooseTasks.js
+++ book_s/mongooseTasks.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testok');
 
-const Book = mongoose.model('Book', { name: String });
+var schema = mongoose.Schema({ name: String })
+
+schema.methods.list = function(){
+    console.log(this.get("name") + " - Лучшая книга")
+}
+    
+const Book = mongoose.model('Book', schema);
 
 const book = new Book({ name: 'Gordost' });
-book.save().then(() => console.log('Перелистывание страниц'));
+book.save().then(() => book.list());
