d13c22bf277ed9a0b46d4f411452ebce20900617
Себия12 <sebiya.bayburtli@gmail.com>
2024-01-14 23:59:30 +0300

    команда записи в COOKIE

--- book_s/routes/index.js
+++ book_s/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
+const List = require("../models/book").List;
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  try {
+    const menu = await List.find({}, { _id: 0, title: 1, nick: 1 });
+
+    res.cookie('greeting', 'Hi!!!').render('index', { 
+      title:'Express', 
+      menu:menu 
+    });
+
+  } catch (err) {
+    next(err);
+  }
 });
      
 module.exports = router;
