12cc7249427c0fcc29c7f5ddc46ee5d0dab0e319
Себия12 <sebiya.bayburtli@gmail.com>
2024-01-14 23:58:26 +0300

    установила модуль EXPRESS-SESSION и настроила COOKIE

--- book_s/routes/books.js
+++ book_s/routes/books.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const List = require("../models/book").List;
-var async = require("async")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -11,20 +10,20 @@ res.send('Новый маршрутизатор, для книг');
 
 
 router.get("/:nick", async (req, res, next) => {
-  try {
-    const [book, books] = await Promise.all([
-      List.findOne({ nick: req.params.nick }),
-      List.find({}, { _id: 0, title: 1, nick: 1 })
-    ]);
-    if (!book) {
-      throw new Error("Нет такой книги");
+      try {
+        const [book, books] = await Promise.all([
+          List.findOne({ nick: req.params.nick }),
+          List.find({}, { _id: 0, title: 1, nick: 1 })
+        ]);
+      if (!book) {
+        throw new Error("Нет такой книги");
+      }
+      renderBook(res, book.title, book.avatar, book.desc, books);
+      
+    } catch (err) {
+      next(err);
     }
-    renderBook(res, book.title, book.avatar, book.desc, books);
-  } catch (err) {
-    next(err);
-  }
-});
-
+  });
 
   function renderBook(res, title, picture, desc, books) {
     console.log(books);
@@ -33,7 +32,7 @@ router.get("/:nick", async (req, res, next) => {
       title: title,
       picture: picture,
       desc: desc,
-      menu: books  // Передаем данные в шаблонизатор EJS с именем menu
+      menu:books
     });
   }
 module.exports = router;
--- book_s/routes/index.js
+++ book_s/routes/index.js
@@ -1,18 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var List = require("../models/book").List
-/* GET home page. */
-router.get('/', async (req, res, next) => {
-  try {
-    const menu = await List.find({}, { _id: 0, title: 1, nick: 1 });
-    res.render('index', {
-      title: 'Express',
-      menu: menu
-    });
-  } catch (err) {
-    next(err);
-  }
-});
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
      
 module.exports = router;
