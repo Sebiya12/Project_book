5d58d5f372c93bc93a589e4427d9ee310b8c9635
Себия12 <sebiya.bayburtli@gmail.com>
2024-01-14 23:50:10 +0300

    создала модели данных приложения

--- book_s/mongooseTasks.js
+++ book_s/mongooseTasks.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/testok');
+mongoose.connect('mongodb://127.0.0.1:27017/testok1');
+var List = require("./models/book").List
 
-var schema = mongoose.Schema({ name: String })
+const book = new List ({
+    title: "Гордость",
+    nick: "Gordost"
+})
 
-schema.methods.list = function(){
-    console.log(this.get("name") + " - Лучшая книга")
-}
-    
-const Book = mongoose.model('Book', schema);
-
-const book = new Book({ name: 'Gordost' });
-book.save().then(() => book.list());
+book.save().then(() => console.log(book.title));
