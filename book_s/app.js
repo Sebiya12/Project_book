576f3885b103544283e86902f1d7eed281282a29
Себия12 <sebiya.bayburtli@gmail.com>
2024-01-15 00:18:51 +0300

    создала сессию

--- book_s/app.js
+++ book_s/app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1/Books')
+//var mongoose = require('mongoose')
+//mongoose.connect('mongodb://127.0.0.1/Books')
 var session = require("express-session")
+var mysql2 = require('mysql2/promise');
+var MySQLStore = require('express-mysql-session')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +15,15 @@ var books = require('./routes/books');
 
 var app = express();
 
+var options = {
+  host : '127.0.0.1',
+  port: '3306',
+  user : 'root',
+  password : '1234',
+  database: 'Books'
+  };
+var connection = mysql2.createPool(options)
+var sessionStore = new MySQLStore( options, connection);
 // view engine setup
 app.engine('ejs',require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
@@ -23,18 +34,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-var MongoStore = require('connect-mongo')
+
+// var MongoStore = require('connect-mongo')
+// app.use(session({
+//   secret: "Book",
+//   cookie:{maxAge:60*1000},
+//   resave: true,
+//   saveUninitialized: true,
+//   store: MongoStore.create({mongoUrl: 'mongodb://127.0.0.1/Books'})
+//   }))
 app.use(session({
-  secret: "Book",
-  cookie:{maxAge:60*1000},
+  secret: 'Book',
+  key: 'sid',
+  store: sessionStore,
   resave: true,
   saveUninitialized: true,
-  store: MongoStore.create({mongoUrl: 'mongodb://127.0.0.1/Books'})
-  }))
+  cookie: { path: '/',
+  httpOnly: true,
+  maxAge: 60*1000
+  }
+  }));
+  
 app.use(function(req,res,next){
     req.session.counter = req.session.counter +1 || 1
     next()
 })
+
 app.use(require("./middleware/createMenu.js"))
 app.use(require("./middleware/createUser.js"))
 app.use('/', indexRouter);
