5231869f06f9e856708b33bf4c2f65f6881ea4eb
Себия12 <sebiya.bayburtli@gmail.com>
2024-01-15 00:17:04 +0300

    вывод данных на экран

--- book_s/routes/books.js
+++ book_s/routes/books.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const List = require("../models/book").List;
-var checkAuth = require("./../middleware/checkAuth.js")
+var db = require('../mySQLConnect.js');
+
+//const List = require("../models/book").List;
+//var checkAuth = require("./../middleware/checkAuth.js")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -10,29 +12,48 @@ res.send('Новый маршрутизатор, для книг');
 
 
 
-router.get("/:nick",checkAuth, async (req, res, next) => {
-      try {
-        const [book, books] = await Promise.all([
-          List.findOne({ nick: req.params.nick }),
-          List.find({}, { _id: 0, title: 1, nick: 1 })
-        ]);
-      if (!book) {
-        throw new Error("Нет такой книги");
-      }
-      renderBook(res, book.title, book.avatar, book.desc, books);
+// router.get("/:nick", async (req, res, next) => {
+//       try {
+//         const [book, books] = await Promise.all([
+//           List.findOne({ nick: req.params.nick }),
+//           List.find({}, { _id: 0, title: 1, nick: 1 })
+//         ]);
+//       if (!book) {
+//         throw new Error("Нет такой книги");
+//       }
+//       renderBook(res, book.title, book.avatar, book.desc, books);
       
-    } catch (err) {
-      next(err);
-    }
-  });
+//     } catch (err) {
+//       next(err);
+//     }
+//   });
 
-  function renderBook(res, title, picture, desc, books) {
-    console.log(books);
+//   function renderBook(res, title, picture, desc, books) {
+//     console.log(books);
   
-    res.render('book', {
-      title: title,
-      picture: picture,
-      desc: desc,
-    });
+//     res.render('book', {
+//       title: title,
+//       picture: picture,
+//       desc: desc,
+//     });
+//   }
+
+  router.get("/:nick", function(req, res, next) {
+    db.query(`SELECT * FROM lists WHERE lists.nick = '${req.params.nick}'`, (err, lists) => {
+    if(err) {
+      console.log(err);
+      if(err) return next(err)
+      } else {
+        if(lists.length == 0) return next(new Error("Книг нет в наличии"))
+          var list = lists[0];
+          res.render('book', {
+            title: list.title,
+            picture: list.avatar,
+            desc: list.about
+    })
   }
+  })
+  
+  });
+  
 module.exports = router;
